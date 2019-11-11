ad588f5197f0e18c5f24d006ad765aa2cd3d86ea
hola <holakt@qq.com>
2019-11-11 10:45:24 +0800

    增加maven的配置文件

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,5 +1,7 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
+        new Random().ints(1, 31).distinct().limit(5).forEach(System.out::println);
     }
 }
